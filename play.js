7fdcd7b06e2b54270f90ac8eafbf14b3530c8181
Mitali <mitali.kawde11@gmail.com>
2023-03-24 01:07:31 -0700

    require setupInput from input.js

--- play.js
+++ play.js
@@ -1,29 +1,8 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-// this function is a 'data' callback handler for stdin
-const handleUserInput = function (key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.stdout.write('\n');
-    process.exit();
-  } /* else {
-    process.stdout.write('.');
-  } */
-};
 
 setupInput();
