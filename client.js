1d0a57966988df36b0a6ef461177b7ff318d5489
Mitali <mitali.kawde11@gmail.com>
2023-03-23 22:33:58 -0700

    After successfully establishing connection with server, printing msg to screen & sending name to the server upon connection

--- client.js
+++ client.js
@@ -10,7 +10,16 @@ const connect = function () {
   // handle incoming data
   conn.on("data", (data) => {
     console.log("Server says: ", data);
-  })
+  });
+
+  // connect event handler
+  // Print a message to the screen when the connection is successfully established
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    
+    // sending a name to the server upon connection
+    conn.write("Name: MSK");
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
