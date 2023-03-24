974683125004eb9a4126ffd616a67a486f917f0d
Mitali <mitali.kawde11@gmail.com>
2023-03-24 12:50:04 -0700

    Refactoring: mapping on movement cmd obj and message obj

--- constants.js
+++ constants.js
@@ -3,9 +3,23 @@
 const IP = "localhost"; //"localhost";  // IP address (host)
 const PORT = 50541 ; // PORT number
 
+const moveCmd = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right"
+};
+
+const message = {
+  x: "Say: go crazy",
+  z: "Say: hungry snake"
+};
+
 
 
 module.exports = {
   IP,
   PORT,
+  moveCmd,
+  message,
 };
